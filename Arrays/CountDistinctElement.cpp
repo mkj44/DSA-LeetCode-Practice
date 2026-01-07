2caa8ced787e6561b62c18d42ac3ada4deeeb13b
Mayank <157869254+mkj44@users.noreply.github.com>
2026-01-07 09:55:30 +0530

    Day 16: Implement CountDistinctElement class method

---
+++ Arrays/CountDistinctElement.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> countDistinct(vector<int> &arr, int k) {
+        int n = arr.size();
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < k; i++) {
+            mp[arr[i]]++;
+        }
+        ans.push_back(mp.size());
+        for (int i = k; i < n; i++) {
+            mp[arr[i - k]]--;
+            if (mp[arr[i - k]] == 0)
+                mp.erase(arr[i - k]);
+            mp[arr[i]]++;
+            ans.push_back(mp.size());
+        }
+        return ans;
+    }
+};
